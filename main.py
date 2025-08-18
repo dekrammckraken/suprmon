e245b61c25d7c6640376a7d0d61ab0250037b34f
dekrammckraken <dekrammckraken>
2025-08-18 14:54:27 +0200

    modular commands

--- main.py
+++ main.py
@@ -3,10 +3,34 @@ import traceback
 from signaler import Signaler
 import sys
 
+
+def exec_command(sig: Signaler):
+  
+    command = sys.argv[1]
+    
+    match command:
+        case "--base":
+            return [sig.base]
+        case "--therm":
+            return [sig.thermal]
+        case "--disks":
+            return [sig.disks_mounts]
+        case "--space":
+            return [sig.disks_spaces]
+        case "--all":
+             return [sig.base, sig.thermal, sig.disks_mounts, sig.disks_spaces]
+        case "--clr":
+             return [sig.clear_all]
+            
+
+
 def main():
     try:
         sig = Signaler()
-        sig.update()
+        actions = exec_command(sig)
+        for action in actions:
+            action()
+     
         sys.exit(0)
     except Exception as e:
         print(traceback.format_exc())
--- signaler.py
+++ signaler.py
@@ -26,61 +26,59 @@ class Signaler:
         except Exception as e:
             print("Failed to send:\n" + traceback.format_exc())
 
-    def clear_all(self):
-        self.signals = [
-            self.emitter.off_signal((2, 0)),
-            self.emitter.off_signal((2, 1)),
-            self.emitter.off_signal((2, 2)),
-            self.emitter.off_signal((2, 3)),
-            self.emitter.off_signal((2, 4)),
-            self.emitter.off_signal((3, 0)),
-            self.emitter.off_signal((3, 1)),
-            self.emitter.off_signal((3, 2)),
-            self.emitter.off_signal((3, 3)),
-            self.emitter.off_signal((3, 4)),
-            self.emitter.off_signal((4, 0)),
-            self.emitter.off_signal((4, 1)),
-            self.emitter.off_signal((4, 2)),
-            self.emitter.off_signal((4, 3)),
-            self.emitter.off_signal((4, 4)),
-            self.emitter.off_signal((5, 0)),
-            self.emitter.off_signal((5, 1)),
-            self.emitter.off_signal((5, 2)),
-            self.emitter.off_signal((5, 3)),
-            self.emitter.off_signal((5, 4)),
-        ]
 
-    def core(self):
+    def clear_all(self):
+        self.clear_row(2)
+        self.clear_row(3)
+        self.clear_row(4)
+        self.clear_row(5)
+        self.send()
+
+    def clear_row(self, row: int):
+        self.signals = [
+            self.emitter.off_signal((row, 0)),
+            self.emitter.off_signal((row, 1)),
+            self.emitter.off_signal((row, 2)),
+            self.emitter.off_signal((row, 3)),
+            self.emitter.off_signal((row, 4)),
+        ]
+        self.send()
+    def base(self):
+        self.clear_row(2)
         self.add_mem_signal(0)
         self.add_mem_signal(1)
         self.add_unused_signal(2)
         self.add_unused_signal(3)
         self.add_unused_signal(4)
+        self.send()
 
-    def therm(self):
+    def thermal(self):
+        self.clear_row(3)
+        self.add_disk_therm_signal(0, 0)
+        self.add_disk_therm_signal(1, 1)
+        self.add_disk_therm_signal(2, 2)
+        self.add_disk_therm_signal(3, 3)
+        self.add_disk_therm_signal(4, 4)
+        self.add_disk_therm_signal(5, 5)
+        self.send()
 
-        self.add_disk_therm_signal(0,0)
-        self.add_disk_therm_signal(1,1)
-        self.add_disk_therm_signal(2,2)
-        self.add_disk_therm_signal(3,3)
-        self.add_disk_therm_signal(4,4)
-        self.add_disk_therm_signal(5,5)
-       
-
-    def disk_mounts(self):
+    def disks_mounts(self):
+        self.clear_row(4)
         self.add_disk_mount_signal("/", 0)
         self.add_disk_mount_signal("/mnt/DataDisk", 1)
         self.add_disk_mount_signal("/mnt/SupportDisk", 2)
         self.add_disk_mount_signal("/mnt/BackupDisk", 3)
         self.add_disk_mount_signal("/mnt/WhiteRabbit", 4)
+        self.send()
 
-    def free_space(self):
-
+    def disks_spaces(self):
+        self.clear_row(5)
         self.add_disk_free_space_signal("/", 0)
         self.add_disk_free_space_signal("/mnt/DataDisk", 1)
         self.add_disk_free_space_signal("/mnt/SupportDisk", 2)
         self.add_disk_free_space_signal("/mnt/BackupDisk", 3)
         self.add_disk_free_space_signal("/mnt/WhiteRabbit", 4)
+        self.send()
 
     def add_disk_mount_signal(self, mount_point, noled):
         self.signals.append(self.emitter.mount_signal(mount_point, (4, noled)))
@@ -93,18 +91,9 @@ class Signaler:
 
     def add_mem_signal(self, noled):
         self.signals.append(self.emitter.mem_signal((2, noled)))
-    
+
     def add_bluetooth_signal(self, noled):
         self.signals.append(self.emitter.bt_signal((2, noled)))
 
     def add_unused_signal(self, noled):
         self.signals.append(self.emitter.unused_signal((2, noled)))
-
-    def update(self):
-        self.clear_all()
-        self.core()
-        self.therm()
-        self.disk_mounts()
-        self.free_space()
-
-        self.send()
